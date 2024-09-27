bc8cd0a83ea58d7b0a0cdfcfedba72b2424c13b5
emiliegorder <emiliegorder@gmail.com>
2024-09-27 14:32:48 -0600

    completed week 2 learning activities

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "America First Federal Credit Union";
+        job1._jobTitle = "Teller";
+        job1._startYear = 2022;
+        job1._endYear = 2024;
+
+        Job job2 = new Job();
+        job2._company = "Kentucky Fried Chicken";
+        job2._jobTitle = "Cook";
+        job2._startYear = 2010;
+        job2._endYear = 2021;
+
+        Resume myresume= new Resume();
+        myresume._name = "Emilie Gorder";
+        myresume._jobs.Add(job1);
+        myresume._jobs.Add(job2);
+
+        myresume.DisplayResume();
     }
 }
